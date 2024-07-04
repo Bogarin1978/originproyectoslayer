23a2a148a8426059a9c4b00a7b985608313d2544
Bogarin1978 <bogarinjorgeramon7@gmail.com>
2024-07-03 23:12:02 -0300

    first commit

---
+++ administradores.php
@@ -0,0 +1,36 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset type="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMINISTRADOR EASY HOUSE</title>
+
+</head>
+
+<link rel="stylesheet"  href="style1.css">
+
+<body>
+
+
+<form action="altasproveedor.php" method=POST>
+<input type=text name=proveedor values="Ingrese proveedor" require>
+<input type=number name=cuit values="ingrese cuit del prov" require>
+<input type=text name=direccion values="Ingrese direccion" require>
+<br><br>
+<input type=submit values="Enviar" require>
+
+<br><br>
+<form action="altasproductos.php" method=POST>
+    <input type=text name=producto values="ingrese el nombre del producto">
+    <inpujt type=number name=codigo values="ingrese el codigo">
+</body>
+</html>
+
+
+
+
