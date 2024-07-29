7b63df004013db335822fecbe20ca79f55b459f9
Bogarin1978 <bogarinjorgeramon7@gmail.com>
2024-07-29 10:48:45 -0300

    resolver

---
+++ logeo/altasclientes.php
@@ -0,0 +1,35 @@
+<?php
+
+require "altasclientes.html";
+require "conectar.php";
+
+
+$rol=$_POST['userol'];
+$Nombre=$_POST['UseNombre'];
+$Apellido=$_POST['useApellido'];
+$email=$_POST['useEmail'];
+$password=$_POST['usepassword'];
+$dir=$_POST['useDireccion'];
+$loc=$_POST['uselocalidad'];
+$prov=$_POST['useprovincia'];
+$telefono=$_POST['usetelefono']; 
+
+
+$sql="INSERT INTO (userol,useNombre,useApellido,useEmail,usepassword,useDireccion,uselocalidad,useprovincia,usetelefono) VALUES ($rol,$Nombre,$email,$password,$dir,$loc,$prov,$telefono)";
+
+$recordset=mysqli_query($conect,$sql);
+
+$register=mysqli_fetch_assoc($recordset);
+echo "rol".$userol."Nombre".$Nombre."Apellido".$Apellido."Email".$email."clave".$password."Direccion".$dir."Localidad".$loc."Provincia".$prov."telefono".$telefono;
+
+
+
+
+
+
+
+
+
+
+
+?>
